02e9778095d01cf6a3ec61e11ca4296bd47d1ad8
KzVerif <kanghunproject@gmail.com>
2026-04-28 12:09:36 +0700

    slip paht

--- bank/isDupe.js
+++ bank/isDupe.js
@@ -1,15 +1,20 @@
 import fs from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
+// กำหนด path แบบ absolute เทียบกับตำแหน่งของไฟล์นี้
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const SLIP_PATH = path.join(__dirname, "slip.json");
 
 export function checkAndSaveSlip(refId) {
-  // โหลดไฟล์ JSON
-  const db = JSON.parse(fs.readFileSync("slip.json", "utf8"));
-  console.log(db.ref);
+  const db = JSON.parse(fs.readFileSync(SLIP_PATH, "utf8"));
+
   if (refId in db.ref) {
-    return { success: false, message: "\`❌\` \`:\` \`สลิปซ้ำ\`" };
+    return { success: false, message: "`❌` `:` `สลิปซ้ำ`" };
   }
 
   db.ref[refId] = true;
-  fs.writeFileSync("slip.json", JSON.stringify(db, null, 2));
-  return { success: true, message: "\`✅\` \`:\` \`สลิปใหม่\`" };
+  fs.writeFileSync(SLIP_PATH, JSON.stringify(db, null, 2));
+  return { success: true, message: "`✅` `:` `สลิปใหม่`" };
 }
-
